ba405aa28506dc727ba2e95c87be173ea335479f
Steby5 <ds6048@student.uni-lj.si>
2022-12-02 10:46:39 +0100

    2.12. changes
    Code revamp.

--- Satellite Predictions/v2/sat_v2.py
+++ Satellite Predictions/v2/sat_v2.py
@@ -1,64 +1,77 @@
 import datetime as dt
 import os
 import requests
-import pandas
 
 OUTPUT_DIR=os.path.dirname(os.path.realpath(__file__))  #curent directory of python file
+temp=os.path.join(OUTPUT_DIR,"temp.txt")    #path to temp.txt file
 tle_txt=os.path.join(OUTPUT_DIR,'tle.txt')  #path to tle.txt file
-temp=os.path.join(OUTPUT_DIR,"temp.txt")
 now = dt.datetime.now() #read system date/time
 
-modifyed=os.path.getmtime(tle_txt)    #get the time since the last update of tle.txt file
-#print(modifyed)
-modifyed_dt = dt.datetime.fromtimestamp(modifyed)   #convert into datetime format
-#print(modifyed_dt)
-diff=now-modifyed_dt   #calculate the difference between now and last modification
-#print(diff)
-diff_sec = diff.total_seconds() #convert the time in seconds
-#print(diff_sec)
-diff_hours = divmod(diff_sec, 3600)[0]    #convert seconds to hours
-#print(diff_hours)
+count=0
+tle=[]
+sateliti = {}
 
-if (diff_hours>=2):    #check if time since last update is less than 2 hours
-    response = requests.get("https://celestrak.org/NORAD/elements/gp.php?GROUP=active&FORMAT=tle")  #send request to celestrak.com to get current TLE data
-    with open(temp,'w') as file:
-        file.write(response.text)   #write data from request function into text file
-    with open(temp, 'r') as r, open(tle_txt, 'w') as o:
+if os.path.isfile(tle_txt):
+    modifyed=os.path.getmtime(tle_txt)    #get the time since the last update of tle.txt file
+    modifyed_dt = dt.datetime.fromtimestamp(modifyed)   #convert into datetime format
+    diff=now-modifyed_dt   #calculate the difference between now and last modification
+    diff_sec = diff.total_seconds() #convert the time in seconds
+    diff_hours = divmod(diff_sec, 3600)[0]    #convert seconds to hours
+else:   #if file does not exists set latest modification time to 0
+    diff = 0
+    diff_sec = 0
+    diff_hours = 0
+
+if (diff_hours>=2 or diff==0):    #check if time since last update is more than 2 hours or 0 (-> create file)
+    response = requests.get("https://celestrak.org/NORAD/elements/gp.php?GROUP=active&FORMAT=tle")  #send request to celestrak.com to get updated TLE data
+    with open(temp,'w+') as file:
+        file.write(response.text)   #write new data from request function into text file
+    with open(temp, 'r') as r, open(tle_txt, 'w') as o: #remove all empty lines in the file
         for line in r:
-            if line.strip():    #remove all empty lines in the file
+            if line.strip():
                 o.write(line)
     os.remove(temp) #delete temp file
 else:
     print("File is stil relavant. No update necessary.")
-tle=pandas.read_csv(tle_txt,sep='\t',header=None)[0].tolist()  #create an array from tle.txt file
 
-#x = slice(0, len(tle), 3)   #first value -> satellitre name
-#y = slice(1, len(tle), 3)   #second value -> TLE line 1
-#z = slice(2, len(tle), 3)   #third value -> TLE line 2
+with open(tle_txt, 'r') as file1:
+    Lines = file1.readlines()
+for line in Lines:
+    count += 1
+    tle.append(line.strip())    #write content from tle.txt to list 'TLE'
 
-#a=tle[x]
-#d = dict.fromkeys(a)
-#print(d)
+tmparr = [0,0]  #create temp array
 
-for i in tle:
-    d=dict.fromkeys(tle[slice(0, len(tle), 3)])
-#    
-#print(d)
+for i in range(0, len(tle), 3):
+    tmparr[0] = tle[i+1]    #set TLE line 1 as first element in tmparr
+    tmparr[1] = tle[i+2]    #set TLE line 2 as second element in tmparr
+    sateliti[tle[i]] = tmparr   #set the satellite name as dict key and other two TLE lines as values for this key
 
-def add_values_in_dict(sample_dict, key, list_of_values):
-    ''' Append multiple values to a key in 
-        the given dictionary '''
-    if key not in sample_dict:
-        sample_dict[key] = list()
-    sample_dict[key].extend(list_of_values)
-    return sample_dict
+class Error(Exception):
+    """Base class for other exceptions"""
+    pass
+class Quit(Error):
+    pass
+class Exists(Error):
+    pass
+class NotSat(Error):
+    pass
 
-word_freq = {'is'   : [1, 3, 4, 8, 10],
-             'at'   : [3, 10, 15, 7, 9],
-             'test' : [5, 3, 7, 8, 1],
-             'this' : [2, 3, 5, 6, 11],
-             'why'  : [10, 3, 9, 8, 12] }
-# Append multiple values for existing key 'at'
-word_freq = add_values_in_dict(d, , [20, 21, 22])
-print('Contents of the dictionary: ')
-print(word_freq)
+while True:
+    try:
+        satellite=input("Input Satellite name (or type 'quit' to end the program): ").upper()   #get user input for satellite name and convert it to uppercase letters
+        if satellite in sateliti:   #check if user input matches satellite name from dict
+            raise Exists    #Raised if user input is a valid satellite name
+        elif satellite == 'QUIT':   #check if user entered 'quit'
+            raise Quit  #Raised when user input equals to 'QUIT'
+        elif satellite not in sateliti: #check if user made a mistake or the specified satellite name is not in the dict
+            raise NotSat   #Raised if user input is NOT a valid satellite name
+        break
+    except Quit:
+        print("Goodbye!")
+        break
+    except Exists:
+        print(sateliti.get(satellite))  #return TLE for that satellite
+        print() #print empty line for better visibility
+    except NotSat:
+        print("This is not a valid satellite name.")    #return error message and allow user to enter different satellite name
