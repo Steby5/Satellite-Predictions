073afb407a5f9af3a87047b740e6ad43506237a8
Steby5 <ds6048@student.uni-lj.si>
2022-12-08 14:39:13 +0100

    Added predictions
    Get user input for:
    - satellite name
    - number of prediction days
    - minimal elevation angle for observation
    
    code decorations :)

--- Satellite Predictions/v2/sat_v2.py
+++ Satellite Predictions/v2/sat_v2.py
@@ -1,6 +1,23 @@
 import datetime as dt
+from datetime import timedelta
 import os
 import requests
+from skyfield.api import wgs84,load,EarthSatellite
+from termcolor import colored
+
+print(colored('                                                                                                                                                                                ','cyan'))
+print(colored('                                                                                                                                                                                ','cyan'))
+print(colored('  /$$$$$$              /$$               /$$ /$$ /$$   /$$                     /$$$$$$$                            /$$ /$$             /$$     /$$                              ','cyan'))
+print(colored(' /$$__  $$            | $$              | $$| $$|__/  | $$                    | $$__  $$                          | $$|__/            | $$    |__/                              ','cyan'))
+print(colored('| $$  \__/  /$$$$$$  /$$$$$$    /$$$$$$ | $$| $$ /$$ /$$$$$$    /$$$$$$       | $$  \ $$  /$$$$$$   /$$$$$$   /$$$$$$$ /$$  /$$$$$$$ /$$$$$$   /$$  /$$$$$$  /$$$$$$$   /$$$$$$$','cyan'))
+print(colored('|  $$$$$$  |____  $$|_  $$_/   /$$__  $$| $$| $$| $$|_  $$_/   /$$__  $$      | $$$$$$$/ /$$__  $$ /$$__  $$ /$$__  $$| $$ /$$_____/|_  $$_/  | $$ /$$__  $$| $$__  $$ /$$_____/','cyan'))
+print(colored(' \____  $$  /$$$$$$$  | $$    | $$$$$$$$| $$| $$| $$  | $$    | $$$$$$$$      | $$____/ | $$  \__/| $$$$$$$$| $$  | $$| $$| $$        | $$    | $$| $$  \ $$| $$  \ $$|  $$$$$$ ','cyan'))
+print(colored(' /$$  \ $$ /$$__  $$  | $$ /$$| $$_____/| $$| $$| $$  | $$ /$$| $$_____/      | $$      | $$      | $$_____/| $$  | $$| $$| $$        | $$ /$$| $$| $$  | $$| $$  | $$ \____  $$','cyan'))
+print(colored('|  $$$$$$/|  $$$$$$$  |  $$$$/|  $$$$$$$| $$| $$| $$  |  $$$$/|  $$$$$$$      | $$      | $$      |  $$$$$$$|  $$$$$$$| $$|  $$$$$$$  |  $$$$/| $$|  $$$$$$/| $$  | $$ /$$$$$$$/','cyan'))
+print(colored(' \______/  \_______/   \___/   \_______/|__/|__/|__/   \___/   \_______/      |__/      |__/       \_______/ \_______/|__/ \_______/   \___/  |__/ \______/ |__/  |__/|_______/ ','cyan'))
+print(colored('                                                                                                                                                                                ','cyan'))
+print(colored('                                                                                                                                                                                ','cyan'))
+print(colored('                                                                                                                                                                                ','cyan'))
 
 OUTPUT_DIR=os.path.dirname(os.path.realpath(__file__))  #curent directory of python file
 temp=os.path.join(OUTPUT_DIR,"temp.txt")    #path to temp.txt file
@@ -10,6 +27,7 @@ now = dt.datetime.now() #read system date/time
 count=0
 tle=[]
 sateliti = {}
+check=0
 
 if os.path.isfile(tle_txt):
     modifyed=os.path.getmtime(tle_txt)    #get the time since the last update of tle.txt file
@@ -31,9 +49,6 @@ if (diff_hours>=2 or diff==0):    #check if time since last update is more than 
             if line.strip():
                 o.write(line)
     os.remove(temp) #delete temp file
-else:
-    print("File is stil relavant. No update necessary.")
-
 with open(tle_txt, 'r') as file1:
     Lines = file1.readlines()
 for line in Lines:
@@ -67,10 +82,44 @@ while True:
             raise NotSat   #Raised if user input is NOT a valid satellite name
         break
     except Quit:
-        print("Goodbye!")
+        print('')
+        print("Goodbye!     (｡◕‿◕｡)'ﾉ''")
         break
     except Exists:
-        print(sateliti.get(usr_input))  #return TLE for that satellite
-        print() #print empty line for better visibility
+        observatory = wgs84.latlon(46.044562, -14.489438)   #our location
+        line1=sateliti.get(usr_input)[0]    #reads the first TLE line
+        line2=sateliti.get(usr_input)[1]    #reads the second TLE line
+        ts = load.timescale()
+        satellite = EarthSatellite(line1, line2, usr_input, ts)
+        while check==0:
+            try:
+                days=int(input("Enter how many days in advance would you like to see the predictions: "))   #get number of days for prediction
+                check=1
+            except ValueError:
+                print("Please enter a valid int number")
+                continue
+        t0 = ts.now()   #set start time to current time
+        t1 = ts.utc(t0.utc_datetime() + timedelta(days))  #set end time to now + x days
+        altitude_degrees = 8.0  #set default value to 8.0 degrees
+        check=0
+        while check==0:
+            try:
+                print(colored('                                   ','white'))
+                print(colored('---___                       ___---','white'))
+                print('   '+colored('/  ','red')+colored('¯¯¯---___     ___---¯¯¯  ','white')+colored('\   ','red'))
+                print('___'+colored('\_','red')+colored('__________\ '+colored('X','yellow')+colored(' /__________','white')+colored('_/','red')+'___'))
+                print('')
+                altitude_degrees = float(input("Enter min. elevation angle for observation: ").upper())    #get user input for altitude limit
+                check=1
+            except ValueError:
+                print("Please enter a valid floating-point number")
+                continue
+        else:
+            t, events = satellite.find_events(observatory, t0, t1, altitude_degrees)    #get all events above our location, for our time window and that are above altitude limit
+            for ti, event in zip(t, events):
+                name = (f'Above {altitude_degrees} degrees -> ','Highest point above -> ',f'Bellow {altitude_degrees} degrees -> ')[event]
+                print(name, ti.utc_strftime('%Y %b %d %H:%M:%S'))
+                if event==2:
+                    print('----------------------------------------')
     except NotSat:
         print("This is not a valid satellite name.")    #return error message and allow user to enter different satellite name
